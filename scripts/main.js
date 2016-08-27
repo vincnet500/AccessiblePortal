b09ca7b623a23d2b8f652253ee2984e0db176065
vincnet500 <vincnet500@gmail.com>
2016-08-27 23:11:03 +0200

    Add radio function and meteo

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,10 @@ http://api.deezer.com/search/album?q=daniel guichard
 http://api.deezer.com/oembed?url=https://www.deezer.com/album/215359
 */
 
+var openWeatherAPPID = "ec336bb0586c8da694f909d8b3d05329";
+var weekdays = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+var months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
+
 // Just to get input parameter from URL (stupid js language !)
 var getQueryParam = function(param) {
     var found;
@@ -55,14 +59,66 @@ function loadPlay() {
 			format: "json"
 		},
 		success: function( response ) {
-			 $("#play-content").html(response.html + getAlbumsLink() + getIndexLink());
+			 $("#play-content").html(getAlbumsLink() + getIndexLink() + response.html);
 		}
 	});
 }
 
 function getIndexLink() {
-    return "<br/><a href=\"index.html\">Autre recherche</a>";
+    return "<a href=\"deezer.html\">Autre recherche</a><br/>";
 }
 function getAlbumsLink() {
-    return "<br/><a href=\"albums.html\">Autre album</a>";
+    return "<a href=\"albums.html\">Autre album</a><br/>";
 }
+
+function loadMeteoNow(searchValue) {
+    $.ajax({
+		url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + openWeatherAPPID + "&units=metric&lang=fr",
+		dataType: "json",
+		data: {
+			format: "json"
+		},
+		success: function(response) {
+            var htmlContent = "";
+            if (typeof(response.weather) != "undefined") {
+                var meteoDescription = response.weather[0].description;
+                var meteoTemperature = response.main.temp;
+                var htmlContent = "Aujourd'hui, en ce moment, " + meteoDescription + ", " + meteoTemperature + " degrés."
+            }
+            $("#meteo-now-content").html(htmlContent);
+		}
+	});
+}
+
+function loadMeteoForecast(searchValue, count) {
+    $.ajax({
+		url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + openWeatherAPPID + "&units=metric&lang=fr",
+		dataType: "json",
+		data: {
+			format: "json"
+		},
+		success: function(response) {
+            var htmlContent = "";
+            var index = 0;
+            var now = new Date();
+            var currentDay = now.getUTCDate();
+            if (typeof(response.list) != "undefined") {
+                for (var x = 0; x < response.list.length; x++) {
+                    var currentForecast = response.list[x];
+                    var cfDate = new Date(1000*currentForecast.dt);
+                    var cfDateDay = cfDate.getUTCDate();
+                    var cdDateDayLabel = weekdays[cfDate.getDay()];
+                    var cfDateMonthLabel = months[cfDate.getMonth()]
+                    var dtTxt = currentForecast.dt_txt;
+                    if (currentDay != cfDateDay && index < count && dtTxt.indexOf("12:00") !== -1) {
+                        htmlContent += cdDateDayLabel + " " + cfDateDay + " " + cfDateMonthLabel + ", " + currentForecast.weather[0].description + ", " + currentForecast.main.temp + " degrés.<br/>"
+                        currentDay = cfDateDay;
+                        index++;
+                    }
+                }
+            }
+            $("#meteo-forecast-content").html(htmlContent);
+		}
+	});
+}
+
